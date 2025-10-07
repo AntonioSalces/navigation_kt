15b58384b0650a4d05c355539c8b01ab60955d82
Antonio Salces Alcaraz <antoniosalces06@gmail.com>
2025-10-07 20:03:10 +0200

    cambio ruta localç

---
+++ app/src/main/java/com/example/myapplication/ui/theme/ToDoDetailScreen.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui.theme
+
+import android.icu.text.CaseMap
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ToDoDetailScreen(
+   title: String,
+
+) {
+    
+}
---
+++ app/src/main/java/com/example/myapplication/ui/theme/ToDoListScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.ui.theme
+
+import androidx.compose.runtime.Composable
+import com.example.myapplication.data.ToDo
+
+@Composable
+fun ToDoListScreen(toDos:List<ToDo>) {
+
+}
