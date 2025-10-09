55eefbad94fe8ebe5c59df5f9a61beddef57b953
Antonio Salces Alcaraz <antoniosalces06@gmail.com>
2025-10-10 00:14:56 +0200

    preparacion

--- app/src/main/java/com/example/myapplication/ui/theme/ToDoCreateScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.ui.theme
-
-import androidx.compose.runtime.Composable
-import com.example.myapplication.data.ToDo
-
-@Composable
-fun ToDoCreateScreen(toDos: MutableList<ToDo>) {
-
-}
